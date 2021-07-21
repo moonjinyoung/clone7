6276a7875518dd5971e2b198d1daacfae052547e
moonjinyoung <wlsdud131@gmail.com>
2021-07-21 20:38:08 +0900

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-
+const authMiddleware = require("../middleware/authmiddleware");
 const { post, sequelize, Sequelize } = require('../models');
 
 var storage = multer.diskStorage({
@@ -17,44 +17,59 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage});
 
-router.get('/', async (req, res) => {
-    const sql = "SELECT p.id, p.content, p.createdAt, p.img, p.user_id, u.name FROM posts AS p JOIN users AS u on p.user_id = u.id";
-    const posts = await sequelize.query(sql, {type:Sequelize.QueryTypes.SELECT});
+router.get('/', authMiddleware, async (req, res, next) => {
+    try {
+        const sql = "SELECT p.id, p.content, p.createdAt, p.img, p.user_id, u.name FROM posts AS p JOIN users AS u on p.user_id = u.id";
+        const posts = await sequelize.query(sql, {type: Sequelize.QueryTypes.SELECT});
 
-    res.send({ posts });
-});
-
-router.post('/post', upload.single("image"), async (req, res, next) => {
-   const { userId } = res.locals.user;
-   const { content } = req.body;
-   const img = `/images/${req.file.filename}`;
-
-   await post.create({ content, img, userId });
-
-   res.send({ result: "success" });
-});
-
-router.put('/:postId', upload.single("image"), async (req, res) => {
-    const { userId } = res.locals.user;
-    const { content } = req.body;
-    const { postId } = req.params;
-    const img = `/images/${req.file.filename}`;
-
-    const isExist = await post.findOne({
-        where: { postId, userId }
-    });
-
-    if(isExist) {
-        isExist.content = content;
-        isExist.img = img;
-        await isExist.save();
-    } else {
-        await post.create({ content, img, userId });
+        res.send({posts});
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
-    res.send({ result: "success" });
 });
 
-router.delete('/:postId', async (req, res) => {
+router.post('/post', authMiddleware, upload.single("image"), async (req, res, next) => {
+   try {
+       const {userId} = res.locals.user;
+       const {content} = req.body;
+       const img = `/images/${req.file.filename}`;
+
+       await post.create({content, img, userId});
+
+       res.send({result: "success"});
+   } catch(err) {
+       console.error(err);
+       next(err);
+   }
+});
+
+router.put('/:postId', authMiddleware, upload.single("image"), async (req, res, next) => {
+    try {
+        const {userId} = res.locals.user;
+        const {content} = req.body;
+        const {postId} = req.params;
+        const img = `/images/${req.file.filename}`;
+
+        const isExist = await post.findOne({
+            where: {postId, userId}
+        });
+
+        if (isExist) {
+            isExist.content = content;
+            isExist.img = img;
+            await isExist.save();
+        } else {
+            await post.create({content, img, userId});
+        }
+        res.send({result: "success"});
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.delete('/:postId', authMiddleware, async (req, res) => {
    const { postId } = req.params;
    const { userId } = res.locals.user;
 
