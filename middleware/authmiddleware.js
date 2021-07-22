b6dadebd796cb49338db711b3577b4c4dc1ca9cc
moonjinyoung <wlsdud131@gmail.com>
2021-07-23 00:49:47 +0900

    updated middleware

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
     console.log(tokenValue)
     const { id } = jwt.verify(tokenValue, "my-secret-key");
     console.log(id)
-    user.findByPk(id).then((id) => {
+    user.findByPk({id}).then((result) => {
       res.locals.user = id;
       console.log('pleaseeeeeee', user, res.locals.user)
       next();
